ebc04c4544bf224872a96ab1c7502a88679d1fe1
ogyogy <47146269+ogyogy@users.noreply.github.com>
2023-06-04 23:37:47 +0900

    メッセージをSQLiteに保存するように変更

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,73 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use chrono::prelude::*;
+use rusqlite::{Connection, Error, Result};
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+struct Message {
+    id: i32,
+    text: String,
+    created_at: i64,
+}
+
+fn conn_db() -> Result<Connection, Error> {
+    let conn = Connection::open("./test.db")?;
+    Ok(conn)
+}
+
+fn get_messages(conn: &Connection) -> Result<Vec<Message>> {
+    let mut stmt = conn.prepare("SELECT id, text, created_at FROM messages")?;
+    let rows = stmt.query_map([], |row| {
+        Ok(Message {
+            id: row.get(0)?,
+            text: row.get(1)?,
+            created_at: row.get(2)?,
+        })
+    })?;
+
+    let mut messages = Vec::new();
+    for msg in rows {
+        messages.push(msg?);
+    }
+
+    Ok(messages)
+}
+
+fn insert_message(conn: &Connection, text: String) -> Result<Message> {
+    let msg = Message {
+        // idは自動的に付与される
+        id: 0,
+        text: text,
+        created_at: Local::now().timestamp(),
+    };
+    conn.execute(
+        "INSERT INTO messages (text, created_at) VALUES (?1, ?2)",
+        (&msg.text, &msg.created_at),
+    )?;
+    Ok(msg)
+}
+
+#[tauri::command]
+fn get_messages_all() -> Result<Vec<Message>, String> {
+    // unwrap()でResultの中身を取得する
+    let conn = conn_db().unwrap();
+    let msgs = get_messages(&conn).unwrap();
+    Ok(msgs)
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> (String, &str) {
-    use chrono::prelude::*;
-    let local: DateTime<Local> = Local::now();
-    let now: String = local.format("%Y-%m-%d %H:%M:%S").to_string();
-    return (now, name);
+fn greet(text: String) -> Message {
+    let conn = conn_db().unwrap();
+    let msg = insert_message(&conn, text).unwrap();
+    return msg;
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_messages_all])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -1,34 +1,61 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
 
-const name = ref("");
-const items = ref([]);
+interface Message {
+  id: number;
+  text: string;
+  created_at: string;
+}
+
+const text = ref("");
+const items = ref<Message[]>([]);
+
+function convert(value: { id: any; text: any; created_at: any; }) {
+  // 生成時刻をUNIX時間からローカルタイムゾーンの文字列に変換
+  // Dateの引数に与えるUNIX時間はミリ秒単位
+  let created_at: Date = new Date(value.created_at * 1000);
+  return {
+    id: value.id,
+    text: value.text,
+    created_at: created_at.toString(),
+  }
+}
+
+onMounted(async () => {
+  let data: { id: number, text: string, created_at: number }[] = await invoke("get_messages_all");
+  // 生成時刻を基準に降順ソート
+  data.sort((a, b) => b.created_at - a.created_at)
+  // 新しく生成したメッセージを上に表示する
+  // 生成時刻をUNIX時間からローカルタイムゾーンの文字列に変換
+  items.value = data.map(value => {
+    return convert(value);
+  })
+})
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  items.value.push(await invoke("greet", { name: name.value }));
-}
-
-async function reload() {
-  items.value.splice(0);
+  let msg: Message = convert(await invoke("greet", { text: text.value }));
+  // 新しく生成したメッセージを上に表示する
+  // 配列の先頭にメッセージを追加
+  items.value.unshift(msg);
 }
 </script>
 
 <template>
   <v-form @submit.prevent="greet()">
-    <v-text-field v-model="name" label="Message" type="input" variant="underlined" prepend-icon="mdi-reload"
-      append-icon="mdi-send" clearable @click:prepend="reload()" @click:append="greet()">
+    <v-text-field v-model="text" placeholder="Write a message" type="input" variant="underlined" append-icon="mdi-send"
+      clearable @click:append="greet()">
     </v-text-field>
   </v-form>
   <v-virtual-scroll :height="400" :items="items">
     <template v-slot:default="{ item }">
       <v-card class="mx-auto">
         <v-card-subtitle>
-          {{ item[0] }}
+          {{ item.created_at }}
         </v-card-subtitle>
         <v-card-text>
-          {{ item[1] }}
+          {{ item.text }}
         </v-card-text>
       </v-card>
     </template>
